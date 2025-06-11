84a98e37d7e4f92b3bf3e0121565f814f441383d
ruby-ist <86733866+ruby-ist@users.noreply.github.com>
2025-06-11 21:37:31 +0530

    Task and gen TCP

--- apps/kv_server/lib/kv_server.ex
+++ apps/kv_server/lib/kv_server.ex
@@ -1,18 +1,44 @@
 defmodule KVServer do
-  @moduledoc """
-  Documentation for `KVServer`.
-  """
+  require Logger
 
-  @doc """
-  Hello world.
+  def accept(port) do
+    # The options below mean:
+    #
+    # 1. `:binary` - receives data as binaries (instead of lists)
+    # 2. `packet: :line` - receives data line by line
+    # 3. `active: false` - blocks on `:gen_tcp.recv/2` until data is available
+    # 4. `reuseaddr: true` - allows us to reuse the address if the listener crashes
+    #
+    {:ok, socket} = :gen_tcp.listen(port, [:binary, packet: :line, active: false, reuseaddr: true])
+    Logger.info("Accepting connections on port #{port}")
+    loop_acceptor(socket)
+  end
 
-  ## Examples
+  defp loop_acceptor(socket) do
+    {:ok, client} = :gen_tcp.accept(socket)
+    {:ok, pid} = Task.Supervisor.start_child(KVServer.TaskSupervisor, fn -> serve(client) end)
+    # if current loop acceptor process crashes, it'll also crash socket connection process (client)
+    # loop acceptor can get restarted, but the existed connections would have gone
+    # By transfering the ownership of socket connection process to the process which serve the request (serve),
+    # a crash in loop acceptor process will not drag down the socket connection process.
+    :ok = :gen_tcp.controlling_process(client, pid)
+    loop_acceptor(socket)
+  end
 
-      iex> KVServer.hello()
-      :world
+  defp serve(socket) do
+    socket
+    |> read_line()
+    |> write_line(socket)
 
-  """
-  def hello do
-    :world
+    serve(socket)
+  end
+
+  defp read_line(socket) do
+    {:ok, data} = :gen_tcp.recv(socket, 0)
+    data
+  end
+
+  defp write_line(line, socket) do
+    :gen_tcp.send(socket, line)
   end
 end
--- apps/kv_server/lib/kv_server/application.ex
+++ apps/kv_server/lib/kv_server/application.ex
@@ -7,9 +7,13 @@ defmodule KVServer.Application do
 
   @impl true
   def start(_type, _args) do
+    port = String.to_integer(System.get_env("PORT") || "4040")
     children = [
-      # Starts a worker by calling: KVServer.Worker.start_link(arg)
-      # {KVServer.Worker, arg}
+      {Task.Supervisor, name: KVServer.TaskSupervisor},
+      # Tasks, by default, have the :restart value set to :temporary, which means they are not restarted.
+      # This means if acceptor crashes, new one will not take its place
+      # So, we need to change that strategy with Supervisor.child_spec/2
+      Supervisor.child_spec({Task, fn -> KVServer.accept(port) end}, restart: :permanent)
     ]
 
     # See https://hexdocs.pm/elixir/Supervisor.html
